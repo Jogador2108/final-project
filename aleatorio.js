2a4b40ea9a306ede1dc57452c18d1b23bc38d772
Jogador2108 <151941911+Jogador2108@users.noreply.github.com>
2025-08-19 22:40:08 -0300

    Create aleatorio.js

---
+++ aleatorio.js
@@ -0,0 +1,12 @@
+export function aleatorio(lista) {
+  const indice = Math.floor(Math.random() * lista.length);
+  return lista[indice];
+}
+
+export const afirmacoes = [
+  "A coragem guia seus passos!",
+  "Seu instinto será posto à prova...",
+  "As sombras escondem perigos e tesouros.",
+  "Quem arrisca pode perder, mas quem não arrisca já perdeu.",
+  "O destino observa cada movimento seu..."
+];
